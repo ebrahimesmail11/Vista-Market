d15db3d685d4e8c33fa26e8ad2f656c9d7640fc2
ebrahim esmail <147981042+ebrahimesmail11@users.noreply.github.com>
2024-09-12 14:18:02 +0300

    Add connectivity check & no network screen

---
+++ lib/src/common/base/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String noNetwork = 'assets/images/no_network.jpg';
+  static const String emptyScreen = 'assets/images/empty_screen.png';
+  static const String pageUnderConstruction =
+      'assets/images/page_under_build.jpg';
+  static const String splash = 'assets/images/splash.jpg';
+}
--- lib/src/utils/vista_market_app.dart
+++ lib/src/utils/vista_market_app.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:vista_market/src/utils/connectivity_controller.dart';
 import 'package:vista_market/src/utils/env.dart';
+import 'package:vista_market/src/utils/no_network_screen.dart';
 
 class VistaMarketApp extends StatelessWidget {
   const VistaMarketApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: Env.instance.depugMode,
-      title: 'Vista Market',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:const  Scaffold(
-        body: Center(
-          child: Text('Vista Market'),
-        ),
-      ),
-    );
+    return ValueListenableBuilder(
+        valueListenable: ConnectivityController.instance.isConnected,
+        builder: (_, value, __) {
+          if (value) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: Env.instance.depugMode,
+              title: 'Vista Market',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, child) {
+                return Scaffold(
+                  body: Builder(builder: (context) {
+                    ConnectivityController.instance.init();
+                    return child!;
+                  },),
+                );
+              },
+              home: const Scaffold(
+                body: Center(
+                  child: Text('Vista Market'),
+                ),
+              ),
+            );
+          } else {
+            return MaterialApp(
+              debugShowCheckedModeBanner: Env.instance.depugMode,
+              title: 'Vista Market',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: const NoNetworkScreen(),
+              );
+          }
+        },);
   }
 }
